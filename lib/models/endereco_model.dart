255534fa9cc035aad6af0820f373847985564dca
Marcelo Zanguettin Pereira <100152421+MarceloZanguettin@users.noreply.github.com>
2025-07-21 20:24:14 -0300

    Update endereco_model.dart

--- lib/models/endereco_model.dart
+++ lib/models/endereco_model.dart
@@ -13,9 +13,11 @@ class Endereco {
     required this.uf,
   });
 
-  factory Endereco.fromJson(Map<String, dynamic> json) {
+  factory Endereco.fromJson(Map<String, dynamic> json) { // 1 - esse construtor factory recebe os dados que decodificamos
+    // Para cada propriedade da nossa classe, ele busca o valor correspondente usando a chave (a string 'cep').
+    // O ?? '' é uma segurança caso a API não retornar uma chave, usamos ums string vazia para evitar que o app quebre
     return Endereco(
-      cep: json['cep'] ?? '',
+      cep: json['cep'] ?? '', 
       logradouro: json['logradouro'] ?? '',
       bairro: json['bairro'] ?? '',
       localidade: json['localidade'] ?? '',
